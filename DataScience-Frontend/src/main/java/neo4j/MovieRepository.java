1f16ff9b2ce1af0824313165a9a0bedde0a35769
Tureczek <55349594+Tureczek@users.noreply.github.com>
2022-05-11 17:53:04 +0200

    Spring and neo4j started

--- DataScience-Frontend/src/main/java/neo4j/MovieRepository.java
+++ DataScience-Frontend/src/main/java/neo4j/MovieRepository.java
@@ -1,2 +1,11 @@
-package neo4j;public interface MovieRepository {
+package neo4j;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import java.util.Collection;
+
+public interface MovieRepository extends Neo4jRepository<Movie, Long> {
+
+    Movie getMovieByTitle(String title);
+
+    Iterable<Movie> findMovieByTitleLike(String title);
 }
