d455114b12895d8ae8a48cde8474754d5461ae3d
Martin Holmqvist <martin-holmqvist@hotmail.com>
2022-05-16 10:58:23 +0200

    Done with visualizations test page

--- DataScience-Frontend/src/main/java/ds/front/Controller/DataController.java
+++ DataScience-Frontend/src/main/java/ds/front/Controller/DataController.java
@@ -1,13 +1,10 @@
 package ds.front.Controller;
 
 import ds.front.Model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/data")
--- DataScience-Frontend/src/main/resources/static/js/visualization.js
+++ DataScience-Frontend/src/main/resources/static/js/visualization.js
@@ -1,6 +1,7 @@
 console.log("Is it working?")
 
-$('#box-1').html('<object type="text/html" data="http://localhost:5000/hi/Martin"/>')
-$('#box-2').html('<object type="text/html" data="http://localhost:5000/hi/Patrick"/>')
-$('#box-3').html('<object type="text/html" data="http://localhost:5000/hi/Nicholas"/>')
-$('#box-4').html('<object type="text/html" data="http://localhost:5000/hi/Dora"/>')
+$('#box-1').html('<object style="width: 100% !important;height: 100% !important;" type="text/html" data="http://localhost:5000/barchart"/>')
+$('#box-2').html('<object style="width: 100% !important;height: 100% !important;" type="text/html" data="http://localhost:5000/piechart"/>')
+$('#box-3').html('<object style="width: 100% !important;height: 100% !important;" type="text/html" data="http://localhost:5000/scatterplot"/>')
+$('#box-4').html('<object style="width: 100% !important;height: 100% !important;" type="text/html" data="http://localhost:5000/pcascatter"/>')
+
