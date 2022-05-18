5fd5a8415ccbc202db9f69ea3ad298e58c629077
Martin Holmqvist <martin-holmqvist@hotmail.com>
2022-05-18 14:46:35 +0200

    Merging with main branch

--- DataScience-Frontend/src/main/java/ds/front/DataScienceFrontendApplication.java
+++ DataScience-Frontend/src/main/java/ds/front/DataScienceFrontendApplication.java
@@ -3,11 +3,9 @@ package ds.front;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
-@SpringBootApplication
 @EnableCaching
-@EnableNeo4jRepositories("ds.front.Neo4J.repository")
+@SpringBootApplication
 public class DataScienceFrontendApplication {
 
     public static void main(String[] args) {
--- DataScience-Frontend/src/main/java/ds/front/Repository/AddressRepository.java
+++ DataScience-Frontend/src/main/java/ds/front/Repository/AddressRepository.java
@@ -2,6 +2,8 @@ package ds.front.Repository;
 
 import ds.front.Model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
